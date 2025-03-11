6e7eaf0a3dba0a57b9def2712294a75017dc2545
s29585 <s29585@pjwstk.edu.pl>
2025-03-11 21:51:15 +0100

    Refactor

--- ConsoleApp1/ConsoleApp1/Calculations.cs
+++ ConsoleApp1/ConsoleApp1/Calculations.cs
@@ -10,9 +10,9 @@ public class Calculations
         }
         
         double sum = 0;
-        foreach (int number in numbers)
+        for (int i = 0; i < numbers.Length; i++)
         {
-            sum += number;
+            sum += numbers[i];
         }
         return sum / numbers.Length;
     }
